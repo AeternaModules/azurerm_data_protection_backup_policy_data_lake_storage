cac5a4c677296c6080d4e947f7a89fdf4e81f6cc
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "data_protection_backup_policy_data_lake_storages_id" {
   description = "Map of id values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.id }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_backup_schedule" {
   description = "Map of backup_schedule values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.backup_schedule }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.backup_schedule if v.backup_schedule != null && length(v.backup_schedule) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_data_protection_backup_vault_id" {
   description = "Map of data_protection_backup_vault_id values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.data_protection_backup_vault_id }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.data_protection_backup_vault_id if v.data_protection_backup_vault_id != null && length(v.data_protection_backup_vault_id) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_default_retention_duration" {
   description = "Map of default_retention_duration values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.default_retention_duration }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.default_retention_duration if v.default_retention_duration != null && length(v.default_retention_duration) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_name" {
   description = "Map of name values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.name }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_retention_rule" {
   description = "Map of retention_rule values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.retention_rule }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.retention_rule if v.retention_rule != null && length(v.retention_rule) > 0 }
 }
 output "data_protection_backup_policy_data_lake_storages_time_zone" {
   description = "Map of time_zone values across all data_protection_backup_policy_data_lake_storages, keyed the same as var.data_protection_backup_policy_data_lake_storages"
-  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.time_zone }
+  value       = { for k, v in azurerm_data_protection_backup_policy_data_lake_storage.data_protection_backup_policy_data_lake_storages : k => v.time_zone if v.time_zone != null && length(v.time_zone) > 0 }
 }
 
